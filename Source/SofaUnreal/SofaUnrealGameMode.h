0569bb724d604aed6d346adcb5436cf940976328
epernod <erik.pernod@belcurves.com>
2017-09-26 16:04:16 +0200

    ADD: default empty C++ project from unreal: source files.

---
+++ Source/SofaUnreal/SofaUnrealGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SofaUnrealGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOFAUNREAL_API ASofaUnrealGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
