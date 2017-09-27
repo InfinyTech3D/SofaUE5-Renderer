abc14908746d61c8f76abb13a5f1b4d679e311bc
epernod <erik.pernod@belcurves.com>
2017-09-27 16:41:17 +0200

    UPDATE: change the import of the dll, use a direct c++ library import inside the project with the api includes.

--- Source/SofaUnreal/SofaUnreal.Build.cs
+++ Source/SofaUnreal/SofaUnreal.Build.cs
@@ -1,14 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+using System.IO;
 using UnrealBuildTool;
+
+using System;
 
 public class SofaUnreal : ModuleRules
 {
-	public SofaUnreal(TargetInfo Target)
-	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
+
+    public SofaUnreal(TargetInfo Target)
+    {
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        loadSofaLib(Target);
 
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
@@ -22,5 +37,37 @@ public class SofaUnreal : ModuleRules
         //			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
         //		}
         // }
+    }
+
+    public bool loadSofaLib(TargetInfo Target)
+    {
+        bool isLibrarySupported = false;
+
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibrarySupported = true;
+
+            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "sofa", "lib");
+
+            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
+            /*
+            test your path with:
+            using System; // Console.WriteLine("");
+            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
+            */
+
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "SofaAdvancePhysicsAPI.lib"));
+        }
+
+        if (isLibrarySupported)
+        {
+            // Include path
+            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "sofa", "include"));
+        }
+
+        Definitions.Add(string.Format("WITH_SOFA_LIB_BINDING={0}", isLibrarySupported ? 1 : 0));
+
+        return isLibrarySupported;
     }
 }
