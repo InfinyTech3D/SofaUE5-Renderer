1e67c2a0f208d2022608df92b8a04e0aade9bafe
epernod <erik.pernod@gmail.com>
2019-01-15 01:09:14 +0100

    [All] CLEAN: restart project under linux. Update default project source files with new verison of UE4.

--- Source/SofaUnreal/SofaUnreal.Build.cs
+++ Source/SofaUnreal/SofaUnreal.Build.cs
@@ -1,74 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-using System.IO;
 using UnrealBuildTool;
-
-using System;
 
 public class SofaUnreal : ModuleRules
 {
-    private string ModulePath
-    {
-        get { return ModuleDirectory; }
-    }
+	public SofaUnreal(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-    private string ThirdPartyPath
-    {
-        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
-    }
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-    public SofaUnreal(TargetInfo Target)
-    {
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-        PrivateDependencyModuleNames.AddRange(new string[] { });
-
-        loadSofaLib(Target);
-
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-        // if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-        // {
-        //		if (UEBuildConfiguration.bCompileSteamOSS == true)
-        //		{
-        //			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-        //		}
-        // }
-    }
-
-    public bool loadSofaLib(TargetInfo Target)
-    {
-        bool isLibrarySupported = false;
-
-        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
-        {
-            isLibrarySupported = true;
-
-            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
-            string LibrariesPath = Path.Combine(ThirdPartyPath, "sofa", "lib");
-
-            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
-            /*
-            test your path with:
-            using System; // Console.WriteLine("");
-            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
-            */
-
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "SofaAdvancePhysicsAPI.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "SofaPhysicsAPI.lib"));
-        }
-
-        if (isLibrarySupported)
-        {
-            // Include path
-            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "sofa", "include"));
-        }
-
-        Definitions.Add(string.Format("WITH_SOFA_LIB_BINDING={0}", isLibrarySupported ? 1 : 0));
-
-        return isLibrarySupported;
-    }
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
 }
