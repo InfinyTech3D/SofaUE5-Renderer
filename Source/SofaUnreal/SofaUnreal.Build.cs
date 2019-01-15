b5740516585ad6186b37690a86cc459be2a470bb
epernod <erik.pernod@gmail.com>
2019-01-15 14:01:54 +0100

    Fix: restore SofaUnreal.Build.cs config file to load sofa lib on windows

--- Source/SofaUnreal/SofaUnreal.Build.cs
+++ Source/SofaUnreal/SofaUnreal.Build.cs
@@ -1,23 +1,71 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+using System.IO;
 using UnrealBuildTool;
+
+using System;
 
 public class SofaUnreal : ModuleRules
 {
-	public SofaUnreal(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+    public SofaUnreal(ReadOnlyTargetRules Target) : base(Target)
+    {
+    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+    PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
+
+    PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        loadSofaLib(Target);
+
+    // Uncomment if you are using Slate UI
+    // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+    // Uncomment if you are using online features
+    // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+    // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
+
+    public bool loadSofaLib(ReadOnlyTargetRules Target)
+    {
+        bool isLibrarySupported = false;
+
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibrarySupported = true;
+
+            //string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "sofa", "lib");
+
+            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
+            /*
+            test your path with:
+            using System; // Console.WriteLine("");
+            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
+            */
+
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "SofaAdvancePhysicsAPI.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "SofaPhysicsAPI.lib"));
+        }
+
+        if (isLibrarySupported)
+        {
+            // Include path
+            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "sofa", "include"));
+        }
+
+        PublicDefinitions.Add(string.Format("WITH_SOFA_LIB_BINDING={0}", isLibrarySupported ? 1 : 0));
+
+        return isLibrarySupported;
+    }
 }
