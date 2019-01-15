d1bb443e69e270a0b147971c34bb629bb443d3b3
epernod <erik.pernod@gmail.com>
2019-01-15 01:07:24 +0100

    [All] CLEAN: restart project under linux. Update default project source files with new verison of UE4.

--- Source/SofaUnreal.Target.cs
+++ Source/SofaUnreal.Target.cs
@@ -5,21 +5,10 @@ using System.Collections.Generic;
 
 public class SofaUnrealTarget : TargetRules
 {
-	public SofaUnrealTarget(TargetInfo Target)
+	public SofaUnrealTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-	}
 
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "SofaUnreal" } );
+		ExtraModuleNames.AddRange( new string[] { "SofaUnreal" } );
 	}
 }
