42c6ac61f128e4c2c9de9abf84c82f80a86cc8ee
epernod <erik.pernod@gmail.com>
2019-01-26 00:23:17 +0100

    [All] Change the complete folder to have SofaUnreal as a plugin and not a project in UE4. Move code

---
+++ Source/SofaUnreal/Private/mytestClass.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "mytestClass.h"
+
+// Sets default values
+AmytestClass::AmytestClass()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AmytestClass::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AmytestClass::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/SofaUnreal/Public/SofaUnreal.h
@@ -0,0 +1,20 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+//DECLARE_LOG_CATEGORY_EXTERN(YourLog, Log, All);
+
+class FSofaUnrealModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+private:
+	/** Handle to the test dll we will load */
+	void*	ExampleLibraryHandle;
+};
