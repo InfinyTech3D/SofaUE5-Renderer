cd57c36c6f9296a6d7bf7e97f9537797309a8ebf
epernod <erik.pernod@gmail.com>
2019-01-28 10:23:38 +0100

    Update build.cs now Sofa lib is added inside the plugin directly

--- Source/SofaUnreal/SofaUnreal.Build.cs
+++ Source/SofaUnreal/SofaUnreal.Build.cs
@@ -1,78 +1,55 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
-using System.IO;
 using UnrealBuildTool;
-
-using System;
 
 public class SofaUnreal : ModuleRules
 {
-    private string ModulePath
-    {
-        get { return ModuleDirectory; }
-    }
-
-    private string ThirdPartyPath
-    {
-        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
-    }
-
-    public SofaUnreal(ReadOnlyTargetRules Target) : base(Target)
-    {
-    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-    PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
-
-    PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-        loadSofaLib(Target);
-
-    // Uncomment if you are using Slate UI
-    // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-    // Uncomment if you are using online features
-    // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-    // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-    }
-
-    public bool loadSofaLib(ReadOnlyTargetRules Target)
-    {
-        bool isLibrarySupported = false;
-
-        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
-        {
-            isLibrarySupported = true;
-
-            //string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
-            string LibrariesPath = Path.Combine(ThirdPartyPath, "sofa", "lib");
-
-            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
-            /*
-            test your path with:
-            using System; // Console.WriteLine("");
-            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
-            */
-
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "SofaAdvancePhysicsAPI.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "SofaPhysicsAPI.lib"));
-        }
-        else
-        {
-            string LibrariesPath = Path.Combine(ThirdPartyPath, "sofa", "lib");
-            Console.WriteLine("LINUX: ... LibrariesPath -> " + LibrariesPath);
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libSofaAdvancePhysicsAPI"));
-            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "sofa", "include"));
-        }
-
-        if (isLibrarySupported)
-        {
-            // Include path
-            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "sofa", "include"));
-        }
-
-        PublicDefinitions.Add(string.Format("WITH_SOFA_LIB_BINDING={0}", isLibrarySupported ? 1 : 0));
-
-        return isLibrarySupported;
-    }
+	public SofaUnreal(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"SofaUnrealLibrary",
+				"Projects",
+                "CoreUObject",
+                "Engine",
+                "InputCore",
+                "ProceduralMeshComponent"
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
 }
