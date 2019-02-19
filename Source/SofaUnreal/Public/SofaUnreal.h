11c3e16718474b1ae05bb6ce8a8cf55126317201
epernod <erik.pernod@gmail.com>
2019-02-19 14:25:39 +0100

    [sources] Add method to convert int into hexa code for debug.

--- Source/SofaUnreal/Public/SofaUnreal.h
+++ Source/SofaUnreal/Public/SofaUnreal.h
@@ -3,12 +3,23 @@
 #pragma once
 
 #include "Modules/ModuleManager.h"
+#include <iomanip>
+#include <string>
+#include <sstream>
+
 DECLARE_LOG_CATEGORY_EXTERN(SUnreal_log, Log, All);
 DEFINE_LOG_CATEGORY(SUnreal_log);
 
 DECLARE_LOG_CATEGORY_EXTERN(SofaLog, Log, All);
 DEFINE_LOG_CATEGORY(SofaLog);
 //DECLARE_LOG_CATEGORY_EXTERN(YourLog, Log, All);
+
+static FString intToHexa(int value)
+{
+    std::stringstream stream;
+    stream << std::hex << value;
+    return FString(stream.str().c_str());
+}
 
 class FSofaUnrealModule : public IModuleInterface
 {
