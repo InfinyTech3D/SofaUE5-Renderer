88a1435d09a3feeb79f1a90ba20bf724f60395dc
epernod <erik.pernod@gmail.com>
2025-06-24 16:17:45 +0200

    Fix SofaVisualMesh to use new API

--- Source/SofaUE5/Private/SofaVisualMesh.cpp
+++ Source/SofaUE5/Private/SofaVisualMesh.cpp
@@ -24,10 +24,12 @@
 #include "SofaVisualMesh.h"
 #include "SofaUE5.h"
 #include "SofaUE5Library/SofaPhysicsAPI.h"
+#include "SofaUE5Library/SofaAdvancePhysicsAPI.h"
 
 // Sets default values
 ASofaVisualMesh::ASofaVisualMesh()
     : m_isStatic(false)
+    , m_inverseNormal(true)
     , m_min(FVector(100000, 100000, 100000))
     , m_max(FVector(-100000, -100000, -100000))
 {
@@ -49,6 +51,8 @@ void ASofaVisualMesh::setSofaMesh(SofaPhysicsOutputMesh* sofaMesh)
 void ASofaVisualMesh::BeginPlay()
 {
     Super::BeginPlay();
+    computeComponent();
+
 }
 
 // This is called when actor is spawned (at runtime or when you drop it into the world in editor)
@@ -67,134 +71,148 @@ void ASofaVisualMesh::PostLoad()
 void ASofaVisualMesh::Tick( float DeltaTime )
 {
     Super::Tick( DeltaTime );
-
+   
     if (!m_isStatic)
+    {
         updateMesh();
+    }
 }
 
 
 void ASofaVisualMesh::updateMesh()
 {   
-    if (m_sofaMesh == nullptr)
+    if (m_sofaAPI == nullptr)
         return;
 
-    //// Get number of vertices
-    //int nbrV = m_sofaMesh->getNbVertices();
+    std::string nodeUniqID = std::string(TCHAR_TO_UTF8(*m_uniqueNameID));
 
-    //// Get the different buffers
-    //float* sofaVertices = new float[nbrV * 3];
-    //float* sofaNormals = new float[nbrV * 3];
-    //m_sofaMesh->getVPositions(sofaVertices);
-    //m_sofaMesh->getVNormals(sofaNormals);
+    // Get number of vertices
+    int nbrV = m_sofaAPI->getNbVertices(nodeUniqID);
 
-    //// Copy data into UE structure
-    //TArray<FVector> vertices;
-    //TArray<FVector> normals;
+    // Get the different buffers
+    float* sofaVertices = new float[nbrV * 3];
+    float* sofaNormals = new float[nbrV * 3];
+    m_sofaAPI->getVPositions(nodeUniqID, sofaVertices);
+    m_sofaAPI->getVNormals(nodeUniqID, sofaNormals);
 
-    //if (m_inverseNormal)
-    //{
-    //    for (int i = 0; i < nbrV; i++)
-    //    {
-    //        vertices.Add(FVector(sofaVertices[i * 3], sofaVertices[i * 3 + 1], sofaVertices[i * 3 + 2]));
-    //        normals.Add(FVector(-sofaNormals[i * 3], -sofaNormals[i * 3 + 1], -sofaNormals[i * 3 + 2]));
-    //    }
-    //}
-    //else
-    //{
-    //    for (int i = 0; i < nbrV; i++)
-    //    {
-    //        vertices.Add(FVector(sofaVertices[i * 3], sofaVertices[i * 3 + 1], sofaVertices[i * 3 + 2]));
-    //        normals.Add(FVector(sofaNormals[i * 3], sofaNormals[i * 3 + 1], sofaNormals[i * 3 + 2]));
-    //    }
-    //}
-    //mesh->UpdateMeshSection(0, vertices, normals, TArray<FVector2D>(), TArray<FColor>(), TArray<FProcMeshTangent>());
+    // Copy data into UE structure
+    TArray<FVector> vertices;
+    TArray<FVector> normals;
+
+    if (m_inverseNormal)
+    {
+        for (int i = 0; i < nbrV; i++)
+        {
+            vertices.Add(FVector(sofaVertices[i * 3], sofaVertices[i * 3 + 1], sofaVertices[i * 3 + 2]));
+            normals.Add(FVector(-sofaNormals[i * 3], -sofaNormals[i * 3 + 1], -sofaNormals[i * 3 + 2]));
+        }
+    }
+    else
+    {
+        for (int i = 0; i < nbrV; i++)
+        {
+            vertices.Add(FVector(sofaVertices[i * 3], sofaVertices[i * 3 + 1], sofaVertices[i * 3 + 2]));
+            normals.Add(FVector(sofaNormals[i * 3], sofaNormals[i * 3 + 1], sofaNormals[i * 3 + 2]));
+        }
+    }
+    mesh->UpdateMeshSection(0, vertices, normals, TArray<FVector2D>(), TArray<FColor>(), TArray<FProcMeshTangent>());
 }
 
 
+void ASofaVisualMesh::computeComponent()
+{
+    createMesh();
+}
+
 void ASofaVisualMesh::createMesh()
 {
-    //UE_LOG(SUnreal_log, Warning, TEXT("### createMesh"));
+    UE_LOG(SUnreal_log, Warning, TEXT("## ASofaVisualMesh::createMesh: %s | UniqueID: %s"), *this->GetName(), *this->m_uniqueNameID);
 
-    //if (m_sofaMesh == nullptr)
-    //    return;
+    if (m_sofaAPI == nullptr)
+        return;
 
-    //// Get topology elements numbers
-    //int nbrV = m_sofaMesh->getNbVertices();
-    //int nbrTri = m_sofaMesh->getNbTriangles();
-    //int nbrQuads = m_sofaMesh->getNbQuads();
-    //UE_LOG(SUnreal_log, Warning, TEXT("## ASofaVisualMesh::createMesh(): nbrV: %d | nbrTri: %d | nbrQuads: %d"), nbrV, nbrTri, nbrQuads);
 
-    //if (nbrV <= 0 || nbrV > 100000)
-    //{
-    //    UE_LOG(SUnreal_log, Error, TEXT("## ASofaVisualMesh::createMesh(): Error on the nbrV returned: %d"), nbrV);
-    //    return;
-    //}
+    std::string nodeUniqID = std::string(TCHAR_TO_UTF8(*m_uniqueNameID));
+    // Get topology elements numbers
+    int nbrV = m_sofaAPI->getNbVertices(nodeUniqID);
+    int nbrTri = m_sofaAPI->getNbTriangles(nodeUniqID);
+    int nbrQuads = m_sofaAPI->getNbQuads(nodeUniqID);
+    UE_LOG(SUnreal_log, Warning, TEXT("## ASofaVisualMesh::createMesh(): nbrV: %d | nbrTri: %d | nbrQuads: %d"), nbrV, nbrTri, nbrQuads);
 
-    //float* sofaVertices = new float[nbrV*3];
-    //float* sofaNormals = new float[nbrV*3];
-    //float* sofaTexCoords = new float[nbrV * 2];
-    //int* sofaTriangles = new int[nbrTri*3];
-    //int* sofaQuads = new int[nbrQuads * 3];
+    if (nbrV <= 0 || nbrV > 100000)
+    {
+        UE_LOG(SUnreal_log, Error, TEXT("## ASofaVisualMesh::createMesh(): Error on the nbrV returned: %d"), nbrV);
+        return;
+    }
 
-    //// Get the different buffers
-    //m_sofaMesh->getVPositions(sofaVertices);
-    //m_sofaMesh->getVNormals(sofaNormals);
-    //m_sofaMesh->getTriangles(sofaTriangles);
-    //m_sofaMesh->getQuads(sofaQuads);
-    //m_sofaMesh->getVTexCoords(sofaTexCoords);
+    float* sofaVertices = new float[nbrV*3];
+    float* sofaNormals = new float[nbrV*3];
+    float* sofaTexCoords = new float[nbrV * 2];
+    int* sofaTriangles = new int[nbrTri*3];
+    int* sofaQuads = new int[nbrQuads * 3];
 
-    //// Convert in Unreal structure
-    //TArray<FVector> vertices;
-    //TArray<int32> Triangles;
-    //TArray<FVector> normals;
-    //TArray<FVector2D> UV0;
-    //TArray<FProcMeshTangent> tangents;
-    //TArray<FLinearColor> vertexColors;
+    // Get the different buffers
+    m_sofaAPI->getVPositions(nodeUniqID, sofaVertices);
+    m_sofaAPI->getVNormals(nodeUniqID, sofaNormals);
+    m_sofaAPI->getTriangles(nodeUniqID, sofaTriangles);
+    m_sofaAPI->getQuads(nodeUniqID, sofaQuads);
+    m_sofaAPI->getVTexCoords(nodeUniqID, sofaTexCoords);
 
-    //for (int i = 0; i < nbrV; i++)
-    //{
-    //    vertices.Add(FVector(sofaVertices[i * 3], sofaVertices[i * 3 + 1], sofaVertices[i * 3 + 2]));
-    //    UV0.Add(FVector2D(sofaTexCoords[i * 2], sofaTexCoords[i * 2 + 1]));
+    UE_LOG(SUnreal_log, Warning, TEXT("## ASofaVisualMesh::createMesh(): TOTO1"));
 
-    //    if (m_inverseNormal)
-    //        normals.Add(FVector(-sofaNormals[i * 3], -sofaNormals[i * 3 + 1], -sofaNormals[i * 3 + 2]));
-    //    else
-    //        normals.Add(FVector(sofaNormals[i * 3], sofaNormals[i * 3 + 1], sofaNormals[i * 3 + 2]));
 
-    //    tangents.Add(FProcMeshTangent(0, 1, 0));
-    //    vertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
-    //}
+    // Convert in Unreal structure
+    TArray<FVector> vertices;
+    TArray<int32> Triangles;
+    TArray<FVector> normals;
+    TArray<FVector2D> UV0;
+    TArray<FProcMeshTangent> tangents;
+    TArray<FLinearColor> vertexColors;
 
-    //// Add triangles
-    //for (int i = 0; i < nbrTri; i++)
-    //{
-    //    Triangles.Add(sofaTriangles[i * 3]);
-    //    Triangles.Add(sofaTriangles[i * 3 + 1]);
-    //    Triangles.Add(sofaTriangles[i * 3 + 2]);
-    //}
+    for (int i = 0; i < nbrV; i++)
+    {
+        vertices.Add(FVector(sofaVertices[i * 3], sofaVertices[i * 3 + 1], sofaVertices[i * 3 + 2]));
+        UV0.Add(FVector2D(sofaTexCoords[i * 2], sofaTexCoords[i * 2 + 1]));
 
-    //// Add quads
-    //for (int i = 0; i < nbrQuads; i++)
-    //{
-    //    Triangles.Add(sofaQuads[i * 4]);
-    //    Triangles.Add(sofaQuads[i * 4 + 1]);
-    //    Triangles.Add(sofaQuads[i * 4 + 2]);
+        if (m_inverseNormal)
+            normals.Add(FVector(-sofaNormals[i * 3], -sofaNormals[i * 3 + 1], -sofaNormals[i * 3 + 2]));
+        else
+            normals.Add(FVector(sofaNormals[i * 3], sofaNormals[i * 3 + 1], sofaNormals[i * 3 + 2]));
 
-    //    Triangles.Add(sofaQuads[i * 4]);
-    //    Triangles.Add(sofaQuads[i * 4 + 2]);
-    //    Triangles.Add(sofaQuads[i * 4 + 3]);
-    //}
-    //
-    //delete[] sofaVertices;
-    //delete[] sofaNormals;
-    //delete[] sofaTexCoords;
-    //delete[] sofaTriangles;
-    //delete[] sofaQuads;
-    //
-    //mesh->CreateMeshSection_LinearColor(0, vertices, Triangles, normals, UV0, vertexColors, tangents, true);
+        tangents.Add(FProcMeshTangent(0, 1, 0));
+        vertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
+    }
 
-    //// Enable collision data
-    ////mesh->ContainsPhysicsTriMeshData(true);
+    // Add triangles
+    for (int i = 0; i < nbrTri; i++)
+    {
+        Triangles.Add(sofaTriangles[i * 3]);
+        Triangles.Add(sofaTriangles[i * 3 + 1]);
+        Triangles.Add(sofaTriangles[i * 3 + 2]);
+    }
+
+    // Add quads
+    for (int i = 0; i < nbrQuads; i++)
+    {
+        Triangles.Add(sofaQuads[i * 4]);
+        Triangles.Add(sofaQuads[i * 4 + 1]);
+        Triangles.Add(sofaQuads[i * 4 + 2]);
+
+        Triangles.Add(sofaQuads[i * 4]);
+        Triangles.Add(sofaQuads[i * 4 + 2]);
+        Triangles.Add(sofaQuads[i * 4 + 3]);
+    }
+    
+    delete[] sofaVertices;
+    delete[] sofaNormals;
+    delete[] sofaTexCoords;
+    delete[] sofaTriangles;
+    delete[] sofaQuads;
+    
+    mesh->CreateMeshSection_LinearColor(0, vertices, Triangles, normals, UV0, vertexColors, tangents, true);
+
+    // Enable collision data
+    //mesh->ContainsPhysicsTriMeshData(true);
 }
 
 void ASofaVisualMesh::computeBoundingBox(const TArray<FVector>& vertices)
